61d1d7cf622ade065e98910bc84f32bdebe3143e
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-01-22 16:15:24 +0530

    minor changes

--- Backend/app.js
+++ Backend/app.js
@@ -63,7 +63,7 @@ async function convertImg(fileName, desiredFormat){
   }
 }
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
   console.log(`Server running on http://localhost:${PORT}`);
 })
