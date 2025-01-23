125f7f6c70480621ace206ad1241a6969f7d75b8
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-01-23 20:21:45 +0530

    updated ui

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,8 +1,43 @@
 const form = document.querySelector('form');
+const selectImgSection = document.querySelector('.select-img-btn');
+const fileInput = document.querySelector('#select-img');
+const imgPreviewSection = document.querySelector('.img-preview');
 
-form.addEventListener('submit', (event) =>{
-  const fileInput = document.querySelector('#select-img');
-  
+// Image preview elements
+const imgPreview = document.querySelector('.img img');
+const imgPreviewName = document.querySelector('.img p');
+const deleteBtn = document.querySelector('.delete-preview');
+
+
+fileInput.addEventListener('change', (event) =>{
+  if (event.target.value === '') return;
+
+  const selectedImg = event.target.files[0];
+  console.log(selectedImg);
+
+  const img = URL.createObjectURL(selectedImg);
+  console.log(img);
+
+  imgPreview.src = img;
+  imgPreviewName.innerText = selectedImg.name;
+
+  imgPreviewSection.style.display = 'flex';
+  selectImgSection.style.display = 'none';
+})
+
+deleteBtn.addEventListener('click', () =>{
+  fileInput.value = '';
+
+  imgPreview.src = '';
+  imgPreviewName.innerText = 'preview.jpg';
+
+  imgPreviewSection.style.display = 'none';
+  selectImgSection.style.display = 'block';
+  console.log(fileInput.value);
+})
+
+
+form.addEventListener('submit', (event) =>{  
   const formData = new FormData(form);
 
   uploadFile(formData);
@@ -13,6 +48,7 @@ form.addEventListener('submit', (event) =>{
       body: formData
     });
 
+    location.reload();
     fileInput.value = '';
   }
 })
